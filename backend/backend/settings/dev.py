eb7454dd68bf840cc25c9cfbe8be9485355869f9
jwun95 <jwun95@naver.com>
2021-11-24 22:33:35 +0900

    backend 생성

---
+++ backend/backend/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-_yic0*(k19e7#an$*b2g2ho&g^js6emv85a!uscp0+&&zw^ie-'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
