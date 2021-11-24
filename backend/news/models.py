73edbeafbd97474f014611f684dc5ab2adc0b482
jwun95 <jwun95@naver.com>
2021-11-25 01:51:39 +0900

    vue image render

--- backend/news/models.py
+++ backend/news/models.py
@@ -3,19 +3,29 @@ from django.db import models
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel, RichTextFieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.images.api.fields import ImageRenditionField
 from wagtail.api import APIField
 
 
 class NewsPage(Page):
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
     
     content_panels = Page.content_panels + [
         FieldPanel('intro'),
-        RichTextFieldPanel('body')
+        RichTextFieldPanel('body'),
+        ImageChooserPanel('image'),
     ]
     
     api_fields = [
         APIField('intro'),
         APIField('body'),
+        APIField('image_thumbnail', serializer=ImageRenditionField('fill-300x300', source='image')),
     ]
